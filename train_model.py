51600a25f205b7be51d7c7c44e03b1713fbc4ae0
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-07-28 10:31:39 +0200

    TASK: Create final txt file for embedding evaluation

--- train_model.py
+++ train_model.py
@@ -1,15 +1,18 @@
 import os
-import json
+import pandas as pd
 from openke.config import Trainer, Tester
 from openke.module.model import TransE
 from openke.module.loss import MarginLoss
 from openke.module.strategy import NegativeSampling
 from openke.data import TrainDataLoader, TestDataLoader
 
+# path to dataset
+data_path = "./benchmarks/FB15K237/"
+n_dimensions = 200
 
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = "./benchmarks/FB15K237/", 
+	in_path = data_path,
 	nbatches = 100,
 	threads = 8, 
 	sampling_mode = "normal", 
@@ -19,13 +22,13 @@ train_dataloader = TrainDataLoader(
 	neg_rel = 0)
 
 # dataloader for test
-test_dataloader = TestDataLoader("./benchmarks/FB15K237/", "link")
+test_dataloader = TestDataLoader(data_path, "link")
 
 # define the model
 transe = TransE(
 	ent_tot = train_dataloader.get_ent_tot(),
 	rel_tot = train_dataloader.get_rel_tot(),
-	dim = 200, 
+	dim = n_dimensions,
 	p_norm = 1, 
 	norm_flag = True)
 
@@ -47,8 +50,18 @@ transe.load_checkpoint('./checkpoint/transe.ckpt')
 tester = Tester(model = transe, data_loader = test_dataloader, use_gpu = True)
 tester.run_link_prediction(type_constrain = False)
 
+# retrieve embedding vectors
+vectors = pd.DataFrame(transe.get_parameters('list')['ent_embeddings.weight'])
+path_to_entities = os.path.join(data_path, 'entity2id.txt')
+entities = pd.read_csv(path_to_entities, sep='\t', skiprows=1, header=None, names=['entity'], usecols=[0])
+entities_with_vectors = pd.merge(left=entities, right=vectors, left_index=True, right_index=True)
+
+# map entities to DBpedia
+dbpedia_mapping = pd.read_csv(os.path.join(data_path, 'dbpedia_mapping.csv.bz2'), sep=' ', compression='bz2')
+entities_with_vectors = pd.merge(left=entities_with_vectors, right=dbpedia_mapping, how='left', left_on='entity', right_on='source')[['target'] + list(range(n_dimensions))]
+
 # persist the model
 if not os.path.exists('results'):
 	os.makedirs('results')
-result_path = 'results/transe_FB15K237.embeddings.json'
-transe.save_parameters(result_path)
+result_path = 'results/transe_FB15K237.embeddings.txt'
+entities_with_vectors.to_csv(result_path, sep=' ', header=False, index=False)
